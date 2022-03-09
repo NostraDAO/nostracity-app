f82a11db836296f9ad5c9c88e1438d0de511e8f6
TheoOliveira <theophilo.oliveira@gmail.com>
2022-03-09 20:56:15 -0300

    as any

--- components/MapPins/index.tsx
+++ components/MapPins/index.tsx
@@ -518,7 +518,7 @@ export default function MapPins() {
           <Input
             placeholder="How many nfts"
             type="number"
-            onChange={(e) => handleQuantity(e.target.value as number)}
+            onChange={(e) => handleQuantity(e.target.value as any)}
           />
         </span>
         {isError && <p style={{ color: "red" }}>{errorMessage}</p>}
