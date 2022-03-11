513683443db03aea9c5cc5ad7b2f76143d7c893f
TheoOliveira <theophilo.oliveira@gmail.com>
2022-03-11 18:37:26 -0300

    fix: remove align form stack on nftlist

--- components/NftList/index.tsx
+++ components/NftList/index.tsx
@@ -65,7 +65,7 @@ export default function NftList({ account }: any) {
   return (
     <Container fixed>
     <Box>
-      <Stack direction="row" spacing={2} align="center">
+      <Stack direction="row" spacing={2}>
         {account ? (
           <>
             <div>
