5b02c1bd612dd341687a1d81eba4cab801cd2dd2
TheoOliveira <theophilo.oliveira@gmail.com>
2022-03-13 23:15:43 -0300

    fix: type nftQuantity

--- components/MapPins/index.tsx
+++ components/MapPins/index.tsx
@@ -52,7 +52,7 @@ export default function MapPins() {
   const [mintPriceBarber, setMintPriceBarber] = useState(0);
   const [mintPriceGrocery, setMintPriceGrocery] = useState(0);
   const [mintPriceDiner, setMintPriceDiner] = useState(0);
-  const [nftQuantity, setNftQuantity] = useState(null);
+  const [nftQuantity, setNftQuantity] = useState();
 
   let totalValueBarber: any;
   let totalValueGrocery: any;
