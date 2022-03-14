c5f147543b1252984cd49a3cb4e8cec9c53010c8
TheoOliveira <theophilo.oliveira@gmail.com>
2022-03-13 23:48:54 -0300

    fix: BN not found on web3 utils types so use require instead of import

--- components/MapPins/index.tsx
+++ components/MapPins/index.tsx
@@ -14,7 +14,8 @@ import groceryContractAbi from "../../abi/GroceryStoreNFT.json";
 import dinerContractAbi from "../../abi/DinerNFT.json";
 
 import NoSsr from "@material-ui/core/NoSsr";
-import Web3 from "web3";
+// import Web3 from "web3";
+const Web3 = require("web3");
 import { useWeb3React } from "@web3-react/core";
 import { AlertModal } from "../AlertModal/AlertModal";
 import Input from "@mui/material/Input";
