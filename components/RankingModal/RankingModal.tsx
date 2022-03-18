7a2f105c856c85af794514ef49ab993d10771701
TheoOliveira <theophilo.oliveira@gmail.com>
2022-03-18 00:52:13 -0300

    added to if statement

--- components/RankingModal/RankingModal.tsx
+++ components/RankingModal/RankingModal.tsx
@@ -133,11 +133,13 @@ export const RankingModal = ({ isOpen, handleClose, title }: any) => {
     await getDinerScore();
     await getBarberScore();
     await getGroceryScore();
-
-    let ar: Scores = [dinerScore, barberScore, groceryScore];
+    if(dinerScore && barberScore && groceryScore ){
+      let ar: Scores = [dinerScore, barberScore, groceryScore];
     ar.sort((a, b ) => b.score - a.score);
     setRankArray([...ar]);
     return ar;
+    }
+    
   }
 
   const TableContent = () => {
